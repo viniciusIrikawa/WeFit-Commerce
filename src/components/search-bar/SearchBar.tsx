dcada0f1ab8aaa20ca2696109d27c32f2d20038e
Vinicius <viniciusiandrade@hotmail.com>
2024-03-28 01:21:33 -0300

    feat: SearchBar component

---
+++ src/components/search-bar/SearchBar.tsx
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react';
+import { InputBar } from './styles';
+
+const SearchBar = () => {
+  return (
+    <InputBar placeholder='Buscar filme pelo nome'/>
+  )
+}
+
+export default SearchBar
---
+++ src/components/search-bar/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const InputBar = styled.input`
+    background-color: #FFFFFF;
+    border-radius: 8px;
+    padding: 16px;
+    width: 1048px;
+    height: 56px;
+`;
