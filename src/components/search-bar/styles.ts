7fde5fe86ece6b0fe297881270782d1131dd6696
Vinicius <viniciusiandrade@hotmail.com>
2024-03-28 01:45:32 -0300

    style: search bar responsiveness

--- src/components/search-bar/styles.ts
+++ src/components/search-bar/styles.ts
@@ -3,7 +3,12 @@ import styled from "styled-components";
 export const InputBar = styled.input`
     background-color: #FFFFFF;
     border-radius: 8px;
+    border: 2px;
     padding: 16px;
-    width: 1048px;
+    width: 90%;
     height: 56px;
+
+    @media (min-width: 768px){
+        width: 1048px;
+    }
 `;
