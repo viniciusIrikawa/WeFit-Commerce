f22296c2d872cda571149d6c2d9a1ce159d52748
Vinicius <viniciusiandrade@hotmail.com>
2024-03-28 01:27:28 -0300

    feat: header responsiveness

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.header`
-    position: absolute;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    width: 1080px;
+    width: 100%;
     height: 88px;
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 24px 16px 24px 16px;
+
+    @media (min-width: 768px){
+        width: 1080px;
+    }
 `;
 
 export const WebsiteName = styled.span`
