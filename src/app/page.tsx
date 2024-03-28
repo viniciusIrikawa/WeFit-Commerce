7266eb31bf5ae71dd0734e9950cb79f17c95c5d8
Vinicius <viniciusiandrade@hotmail.com>
2024-03-28 01:35:26 -0300

    style: centralizing components in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header/Header";
+import SearchBar from "@/components/search-bar/SearchBar";
 
 export default function Home() {
   return (
     <main>
       <Header/>
+      <SearchBar/>
     </main>
   );
 }
