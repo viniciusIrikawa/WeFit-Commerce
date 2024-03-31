bb7fa3bcbe3f74be8481a73c53e5d1c5cf51d5f9
Vinicius <viniciusiandrade@hotmail.com>
2024-03-31 00:33:19 -0300

    feat: subtotal calcuation

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { 
   CartContainer, 
   Table, 
@@ -25,7 +25,8 @@ import EmptyCart from '@/components/cart/empty/EmptyCart';
 
 const page = () => {
   const { cartItems, setCartItems } = useContext(ProductContext);
-  
+ let subTotalCart = 0;
+
   const removeItem = (productID: number | undefined) => {
     const items:IProduct[] = cartItems.filter(item => item.id !== productID);
     setCartItems(items); 
@@ -62,38 +63,41 @@ const page = () => {
             </tr>
           </thead>
           <tbody>
-            {cartItems.map((item: IProduct) => (
-              <tr>
-                <TableData> 
-                  <ProductWrapper>
-                    <Image src={item.image} alt='' width={91} height={114}/> 
-                    <TitlePriceWrapper>
-                      <Title>
-                        {item.title}
-                      </Title>
-                      <Price>
-                        R$ {item.price}
-                      </Price>
-                    </TitlePriceWrapper>
-                  </ProductWrapper>
-                </TableData>
-                <TableData>
-                  <QuantityWrapper>
-                    <BtnQuantity onClick={() => decrementQuantity(item.id)}> 
-                      <Image src={'/img/btn_decrement.png'} alt='Decrement button' width={14} height={14}/>
-                    </BtnQuantity>
-                    <Quantity value={item.quantity}/>
-                    <BtnQuantity onClick={() => incrementQuantity(item.id)}> 
-                      <Image src={'/img/btn_increment.png'} alt='Increment button' width={14} height={14}/>
-                    </BtnQuantity>
-                  </QuantityWrapper>
-                </TableData>
-                <TableData> {((item?.quantity ?? 0) * item?.price).toFixed(2)} </TableData>
-                <TableData>
-                  <Image src={'/img/trash.png'} onClick={() => removeItem(item.id)} alt='Trash button' width={18} height={18}/>
-                </TableData>
-              </tr>
-            ))}
+            {cartItems.map((item: IProduct) => {
+              subTotalCart += (item?.quantity ?? 0) * item?.price
+              return (  
+                <tr>
+                  <TableData> 
+                    <ProductWrapper>
+                      <Image src={item.image} alt='' width={91} height={114}/> 
+                      <TitlePriceWrapper>
+                        <Title>
+                          {item.title}
+                        </Title>
+                        <Price>
+                          R$ {item.price}
+                        </Price>
+                      </TitlePriceWrapper>
+                    </ProductWrapper>
+                  </TableData>
+                  <TableData>
+                    <QuantityWrapper>
+                      <BtnQuantity onClick={() => decrementQuantity(item.id)}> 
+                        <Image src={'/img/btn_decrement.png'} alt='Decrement button' width={14} height={14}/>
+                      </BtnQuantity>
+                      <Quantity value={item.quantity}/>
+                      <BtnQuantity onClick={() => incrementQuantity(item.id)}> 
+                        <Image src={'/img/btn_increment.png'} alt='Increment button' width={14} height={14}/>
+                      </BtnQuantity>
+                    </QuantityWrapper>
+                  </TableData>
+                  <TableData> {((item?.quantity ?? 0) * item?.price).toFixed(2)} </TableData>
+                  <TableData>
+                    <Image src={'/img/trash.png'} onClick={() => removeItem(item.id)} alt='Trash button' width={18} height={18}/>
+                  </TableData>
+                </tr>
+              )
+            })}
           </tbody>
         </Table>
         <Divisor/>
@@ -101,7 +105,7 @@ const page = () => {
           <BtnCheckout href={'/success'}> Finalizar Pedido </BtnCheckout>
           <div>
             <TxtTotal> Total </TxtTotal>
-            <TotalPrice> R$ 29,99</TotalPrice>
+            <TotalPrice> R$ {subTotalCart.toFixed(2)} </TotalPrice>
           </div>
         </TotalWrapper>
       </CartContainer>
