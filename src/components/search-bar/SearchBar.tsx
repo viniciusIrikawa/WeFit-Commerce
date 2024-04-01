60dbfea93cf8d0ef02ebb71479427a41bc2dd8ee
Vinicius <viniciusiandrade@hotmail.com>
2024-04-01 13:35:19 -0300

    feat: function to search movie

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -1,13 +1,31 @@
 'use client';
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { BtnSearch, InputBar, WrapperInputBar } from './styles';
 import Image from 'next/image';
+import { PRODUCTS } from '@/services/axiosBaseUrl';
+import { ProductContext } from '@/Context/ContextProducts';
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const { setProducts } = useContext(ProductContext);
+
+  const searchMovie = async () => {
+    try {
+      const response = await PRODUCTS.get(`/products`, {
+        params: {
+          title: searchTerm,
+        }
+      });
+      setProducts(response.data);
+    } catch (error) {
+      console.log('Could not find the movie.');
+    }
+  };
+
   return (
     <WrapperInputBar>
-      <InputBar placeholder='Buscar filme pelo nome'/>
-      <BtnSearch> <Image src='/img/loup.png' alt='Cart icon' width={18} height={18}/> </BtnSearch>
+      <InputBar placeholder='Buscar filme pelo nome' onChange={(e) => setSearchTerm(e.target.value)}/>
+      <BtnSearch onClick={searchMovie}> <Image src='/img/loup.png' alt='Cart icon' width={18} height={18}/> </BtnSearch>
     </WrapperInputBar>
   )
 }
