95c09f527e8a868b70dedb9bf910da43e52c8668
Vinicius <viniciusiandrade@hotmail.com>
2024-04-01 14:05:17 -0300

    feat: reload page

--- src/components/cart/empty/EmptyCart.tsx
+++ src/components/cart/empty/EmptyCart.tsx
@@ -1,13 +1,21 @@
 import Image from 'next/image';
 import React from 'react';
 import { Container, H1, BtnBack } from './styles';
+import { useRouter } from 'next/navigation';
 
 const EmptyCart = () => {
+
+  const router = useRouter();
+
+  const reloadPage = () => {
+    router.refresh();
+  };
+
   return (
     <Container>
         <H1> Parece que não há nada por aqui :( </H1>
         <Image src={'/img/empty_cart.png'} alt='Compra realizada com sucesso' width={447} height={265.36}/>
-        <BtnBack href='/cart'> Recarregar Página </BtnBack>
+        <BtnBack onClick={reloadPage} href='/cart'> Recarregar Página </BtnBack>
     </Container>
   )
 }
